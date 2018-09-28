0df28afbcafdcfe9c128860a537bb189adab060a
BraulioMercado <43398760+BraulioMercado@users.noreply.github.com>
2018-09-28 00:48:56 -0700

    Add files via upload

---
+++ E2.1-Factorial(for)/E2.1-Factorial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2._1_Factorial
+{
+    class Program
+    {
+        public int Operaciones()
+        {
+            int Fact = 1;
+            int Cont;
+            for (Cont = 1; Cont <= 6; Cont++)
+            {
+                Fact = Fact * Cont;
+            }
+            return Fact;
+        }
+        static void Main(string[] args)
+        {
+            DateTime tiempo1 = DateTime.Now;
+            Program Fact = new Program();
+            Console.WriteLine("El resultado es {0} ", Fact.Operaciones());
+            DateTime tiempo2 = DateTime.Now;
+            TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+            Console.Write("TIEMPO: " + total.ToString());
+            Console.ReadKey();
+        }
+    }
+}
